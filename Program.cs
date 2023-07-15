d1d21d5077a6cea32abc84808946ecf256839261
developerMathi <mathins160@gmail.com>
2023-07-15 15:42:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using ConvertCurrencyClient;
+using Grpc.Net.Client;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Welcome to currency converter");
+        Console.ReadLine();
+        using var channel = GrpcChannel.ForAddress("https://localhost:7263");
+        var client= new Greeter.GreeterClient(channel);
+        var reply = await client.SayHelloAsync(new HelloRequest { Name = "CConverter" });
+        Console.WriteLine($"Greetings{reply.Message}");
+        Console.ReadLine(); 
+    }
+}
